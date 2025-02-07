9d1b7c8c2d42fc6fef3c9d2274c897c42ab8074f
Loke-Automation <lokeshkumar1212@gmail.com>
2025-02-07 22:15:45 +0530

    Add files via upload
    Palindrome

---
+++ Palidrome.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class Palidrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int input, output = 0;
+		
+		System.out.println(121);
+		input=
+		
+		for (i=0;)
+
+	}
+
+}
